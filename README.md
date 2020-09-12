f04518cb8db14fd3f4d9fc809bc3a47bfe07645e
AKSHAI SRINIVASAN S <akshai@AKSHAIs-MacBook-Pro.local>
2020-09-12 19:38:53 +0530

    Report added to the project

--- README.md
+++ README.md
@@ -5,31 +5,39 @@ We use [Double DQN technique](https://arxiv.org/abs/1509.06461) to train the age
 
 # Environment Setup
 
-To set up your python environment to run the code in this repository, follow the instructions below.
+We utilize [Unity Machine Learning Agents](https://github.com/Unity-Technologies/ml-agents) plugin to interact with the environment. 
+To set up your python environment to run the code in this repository, follow the instructions below:
 
-Create (and activate) a new conda environment with Python 3.6.
-```bash
-conda create --name <env_name> python=3.6
-conda activate <env_name>
-pip install gym
-```
+1. Create (and activate) a new [Conda](https://docs.anaconda.com/anaconda/install/) environment with Python 3.6.
 
-Clone the repository, and navigate to the python/ folder. Then, install several dependencies.
-```bash
-git clone https://github.com/Akshai/navigation-rl.git
-cd python
-pip install .
-```
+    ```bash
+    conda create --name <env_name> python=3.6
+    conda activate <env_name>
+    ```
+
+
+2. Clone the repository, and navigate to the python/ folder. Then, install several dependencies.
+
+    ```bash
+    git clone https://github.com/Akshai/navigation-rl.git
+    cd navigation-rl/python
+    pip install .
+    ```
 
 # Training
+
+From the home path **navigation-rl**, run the following command to train the agent:
 
 ```bash
 python train_navigation.py
 ```
 
 # Visualization
+
+From the home path **navigation-rl**, run the following command to visualize the trained agent:
+
 ```bash
-python visualize_navigation.py --model_path=<Path to your trained checkpoint>
+python visualize_navigation.py <Path to your trained checkpoint>
 ```
 
 
--- train_navigation.py
+++ train_navigation.py
@@ -12,7 +12,6 @@ env = UnityEnvironment(file_name="Banana.app")
 
 # get the default brain
 brain_name = env.brain_names[0]
-brain = env.brains[brain_name]
 
 def dqn(args, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
     """Deep Q-Learning.
@@ -33,6 +32,7 @@ def dqn(args, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
     action_size = 4
     agent = Agent(state_size, action_size,1)
     for i_episode in range(1, args.num_episodes+1):
+        #resetting the environment for a new episode
         env_info = env.reset(train_mode=True)[brain_name]
         state = env_info.vector_observations[0]  
         score = 0
@@ -49,12 +49,11 @@ def dqn(args, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
             cnt+=1
             if done:
                 break
-        scores_window.append(score)       # save most recent score
+        scores_window.append(score)       # save most recent score in the 100 episode window
         scores.append(score)              # save most recent score
         eps = max(eps_end, eps_decay*eps) # decrease epsilon
         print('\rEpisode {}\tAverage Score in the last 100 episodes: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
-        if i_episode % 10 == 0:
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if i_episode % args.save_every == 0:
             print('\nSaving Checkpoint for {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
             torch.save(agent.qnetwork_local.state_dict(), os.path.join(args.save_checkpoint_path,'checkpoint_'+str(i_episode)+'.pth'))
     return scores
@@ -77,7 +76,7 @@ def get_args():
     parser.add_argument('--num_episodes', type=int, default=2000, help='Path to the folder of input images')
     parser.add_argument('--save_every', type=int, default=100, help='Frequency with which we are going to save the checkpoints')
     parser.add_argument('--reward_plot_path', type=str,  default="./", help='Path to store the reward plot')
-    parser.add_argument('--save_checkpoint_path', default="./checkpoints", help ='Path to store the trained model')
+    parser.add_argument('--save_checkpoint_path', default="./", help ='Path to store the trained model')
     return parser.parse_args()
 
 if __name__ == '__main__':
